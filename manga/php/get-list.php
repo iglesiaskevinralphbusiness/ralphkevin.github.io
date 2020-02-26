9e609b67399ba3b1b7f896f24112d698b3f776d2
kevinralph <watashiwaarigato@gmail.com>
2020-02-26 20:54:42 +0800

    manga update

--- manga/php/get-list.php
+++ manga/php/get-list.php
@@ -1,10 +1,29 @@
 <?php
-    $list = "./json/list.json";
-    $chapters = "./json/chapters.json";
 
-    $myfile = fopen($list, "r") or die("Unable to open file!");
-    $response_list = fread($myfile,filesize($list));
-    fclose($myfile);
+    if(!isset($_SERVER['PHP_AUTH_USER'])){
+        header("WWW-Authenticate: Basic realm=\"Private Area\"");
+        header("HTTP/1.0 401 Unauthorized");
+        print "Sorry, you need proper credentials";
+    }
+    else {
+        if(($_SERVER['PHP_AUTH_USER'] == 'mangabay' && ($_SERVER['PHP_AUTH_PW'] == 'mangabay'))){
+            $list = "./json/list.json";
+            $chapters = "./json/chapters.json";
+        
+            $myfile = fopen($list, "r") or die("Unable to open file!");
+            $response_list = fread($myfile,filesize($list));
+            fclose($myfile);
+            print $response_list;
+        }
+        else {
+            header("WWW-Authenticate: Basic realm=\"Private Area\"");
+            header("HTTP/1.0 401 Unauthorized");
+            print "Sorry, you need proper credentials";
+        }
+    }
+
+
+    
 
     
 ?>
