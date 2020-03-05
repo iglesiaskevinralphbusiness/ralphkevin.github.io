5dad52aad447f4e9782f7b09d26e41190766bc62
kevinralph <kevin.iglesias@r4pid.com>
2020-03-05 11:39:34 +0800

    update manga

--- manga/m/src/get-impressions.js
+++ manga/m/src/get-impressions.js
@@ -3,60 +3,27 @@ const expect = require('chai').expect;
 const fs = require('fs');
 
 (async () => {
-    let browser;
-    let page;
-    browser = await puppeteer.launch({
-        headless: false,       //for debuging
-        slowMo: 0,             //delay beetween puppeteer actions
-        devtools: false,       //opens developer tools to browser
-        timeout: 10000,         //max time of the browser to launch
-    });
-    page = await browser.newPage();
-    await page.setViewport({
-        width: 800,
-        height: 600
-    });
+    for(i=0; i<=100; i++){
 
-    m_list = JSON.parse(await fs.readFileSync(__dirname + '/xml/sitemap_index.json', 'utf8'));
-
-
-    const checkip = 'https://proxybot.io/api/v1/QkCgBmZ9OChzXSgIFgmIjO0pFgm2?url=https://whatismyipaddress.com/';
-    await page.goto(checkip, {timeout: 180000});
-
-    for (let i = 0; i < m_list.length; i++) {
-        const prox = 'https://proxybot.io/api/v1/QkCgBmZ9OChzXSgIFgmIjO0pFgm2?url=';
-        const url = m_list[i];
-        const path = prox + url;
-
-        await page.goto(path, {timeout: 180000});
-        await page.waitForNavigation({
-            waitUntil: 'networkidle0',
+        let browser;
+        let page;
+        browser = await puppeteer.launch({
+            headless: false,       //for debuging
+            slowMo: 0,             //delay beetween puppeteer actions
+            devtools: false,       //opens developer tools to browser
+            timeout: 10000,         //max time of the browser to launch
+            args: ['--incognito',],
         });
-        await autoScroll(page);
-        const rand = 0 + Math.random() * (3000 - 0);
-        await page.waitFor(rand);
-        console.log(url);
+       [page] = await browser.pages();
+        await page.setViewport({
+            width: 800,
+            height: 600
+        });
+        await page.goto('http://mangariot.com/ads/all.html', { waitUntil: 'load', timeout: 0 });
+        await page.waitForSelector('body', { waitUntil: ["networkidle0", "domcontentloaded"], timeout: 0 });
+        console.log(`browser: ${i + 1}`);
+        await page.waitFor(400000);
+
+        await browser.close();
     }
-
-    await browser.close();
 })();
-
-
-async function autoScroll(page){
-    await page.evaluate(async () => {
-        await new Promise((resolve, reject) => {
-            var totalHeight = 0;
-            var distance = 100;
-            var timer = setInterval(() => {
-                var scrollHeight = document.body.scrollHeight;
-                window.scrollBy(0, distance);
-                totalHeight += distance;
-
-                if(totalHeight >= scrollHeight){
-                    clearInterval(timer);
-                    resolve();
-                }
-            }, 100);
-        });
-    });
-}
