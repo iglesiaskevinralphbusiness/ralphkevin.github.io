19414dc05a9e5142c58c8b5b38e0fa21822edb8a
kevinralph <watashiwaarigato@gmail.com>
2020-03-06 04:12:23 +0800

    update manga

--- manga/m/src/get-impressions.js
+++ manga/m/src/get-impressions.js
@@ -1,29 +1,34 @@
-const puppeteer = require('puppeteer');
-const expect = require('chai').expect;
-const fs = require('fs');
+const puppeteer = require("puppeteer");
+const expect = require("chai").expect;
+const fs = require("fs");
 
 (async () => {
-    for(i=0; i<=100; i++){
+  for (i = 0; i <= 300; i++) {
+    let browser;
+    let page;
+    browser = await puppeteer.launch({
+      headless: false, //for debuging
+      slowMo: 0, //delay beetween puppeteer actions
+      devtools: false, //opens developer tools to browser
+      timeout: 10000, //max time of the browser to launch
+      args: ["--incognito"]
+    });
+    [page] = await browser.pages();
+    await page.setViewport({
+      width: 800,
+      height: 600
+    });
+    await page.goto("http://mangariot.com/ads/all.html", {
+      waitUntil: "load",
+      timeout: 0
+    });
+    await page.waitForSelector("body", {
+      waitUntil: ["networkidle0", "domcontentloaded"],
+      timeout: 0
+    });
+    console.log(`browser: ${i + 1}`);
+    await page.waitFor(400000);
 
-        let browser;
-        let page;
-        browser = await puppeteer.launch({
-            headless: false,       //for debuging
-            slowMo: 0,             //delay beetween puppeteer actions
-            devtools: false,       //opens developer tools to browser
-            timeout: 10000,         //max time of the browser to launch
-            args: ['--incognito',],
-        });
-       [page] = await browser.pages();
-        await page.setViewport({
-            width: 800,
-            height: 600
-        });
-        await page.goto('http://mangariot.com/ads/all.html', { waitUntil: 'load', timeout: 0 });
-        await page.waitForSelector('body', { waitUntil: ["networkidle0", "domcontentloaded"], timeout: 0 });
-        console.log(`browser: ${i + 1}`);
-        await page.waitFor(400000);
-
-        await browser.close();
-    }
+    await browser.close();
+  }
 })();
